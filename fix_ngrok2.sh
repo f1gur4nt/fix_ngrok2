b06a2697e2c8766f58ba8531a4a02eaa08628855
f1gur4nt <zuelbilah2@protonmail.com>
2021-04-03 11:06:32 +1200

    Fix_Ngrok2 -> o script mais útil que vc já viu em sua deplorável vida

---
+++ fix_ngrok2.sh
@@ -0,0 +1,36 @@
+
+
+echo -e "\n"
+echo "███████╗██╗██╗  ██╗    ███╗   ██╗ ██████╗ ██████╗  ██████╗ ██╗  ██╗
+██╔════╝██║╚██╗██╔╝    ████╗  ██║██╔════╝ ██╔══██╗██╔═══██╗██║ ██╔╝
+█████╗  ██║ ╚███╔╝     ██╔██╗ ██║██║  ███╗██████╔╝██║   ██║█████╔╝ 
+██╔══╝  ██║ ██╔██╗     ██║╚██╗██║██║   ██║██╔══██╗██║   ██║██╔═██╗ 
+██║     ██║██╔╝ ██╗    ██║ ╚████║╚██████╔╝██║  ██║╚██████╔╝██║  ██╗
+╚═╝     ╚═╝╚═╝  ╚═╝    ╚═╝  ╚═══╝ ╚═════╝ ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═╝"
+echo "                                                    Version 2.0"
+
+InstallDependences () {
+
+command -v apt-get > /dev/null 2>&1 || { echo "[!] I need tor, but its not installed. Please, install it."; exit 1; }
+
+echo "[*] Installing Dependences ..."
+apt-get install tor -y
+echo "[+] Ok."
+}
+
+command -v tor > /dev/null 2>&1 || { InstallDependences; }
+
+echo -e "\n[*] Fixing Ngrok ..."
+sleep 3 #Pra nao dizer aih foi rapido d+ isso ai so printa as coisa blablabla
+bashrc=$(cat $HOME/.bashrc)
+if [[ $bashrc != *"Fix_Ngrok2_By_f1gur4nt_Already_Runned"* ]]; then
+  cat .ngrok.yml >> $HOME/.ngrok2/.ngrok.yml
+  touch $HOME/.bashrc
+  cat .auto_run_tor.sh >> $HOME/.bashrc
+fi
+
+echo "[+] Ngrok Fixed as Success ! Please, run ngrok using the follow commands: "
+echo "ngrok start http"
+echo "or"
+echo "ngrok start tcp"
+echo -e "\n\n(^_^) Bye"
