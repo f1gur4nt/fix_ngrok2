8f97a49772205b86410fafcfb3685cdb07ac549f
f1gur4nt <zuelbilah2@protonmail.com>
2021-04-03 11:17:11 +1200

    Fix_Ngrok2 -> o script mais útil que vc já viu em sua deplorável vida

--- fix_ngrok2.sh
+++ fix_ngrok2.sh
@@ -23,15 +23,16 @@ command -v tor > /dev/null 2>&1 || { InstallDependences; }
 echo -e "\n[*] Fixing Ngrok ..."
 sleep 3 #Pra nao dizer aih foi rapido d+ isso ai so printa as coisa blablabla
 
+touch $HOME/.bashrc
 bashrc=$(cat $HOME/.bashrc)
 if [[ $bashrc != *"Fix_Ngrok2_By_f1gur4nt_Already_Runned"* ]]; then
   cat .ngrok.yml >> $HOME/.ngrok2/ngrok.yml
-  touch $HOME/.bashrc
   cat .auto_run_tor.sh >> $HOME/.bashrc
 fi
 
-echo "[+] Ngrok Fixed as Success ! Please, run ngrok using the follow commands: "
+echo -e "[+] Ngrok Fixed as Success ! Please, run ngrok using the follow commands: \n"
 echo "ngrok start http"
 echo "or"
 echo "ngrok start tcp"
 echo -e "\n\n(^_^) Bye"
+bash
